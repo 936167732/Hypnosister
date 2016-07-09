a309f9936c48bce56c9982106268b6e7eced3479
谢海邦 <X@xiehaibangdemacbook-pro.local>
2016-07-09 16:09:56 +0800

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by 谢海邦 on 16/7/9.
+//  Copyright © 2016年 谢海邦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
